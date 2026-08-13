fdb2aa42920fa99f599c5b13fe04ecfde912810f
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-13 17:48:55 +0300

    second commit

---
+++ level4/homework.py
@@ -0,0 +1,35 @@
+
+name=input ("enter your name: " )
+print("name")
+
+color=input("enter youre favorit color: ")
+print("color")
+
+f-string  aris tekstis martivad chasanacvleli operatori
+
+im 11 years old
+age=11
+print(im{"age"}years old)
+
+my surename is kvinikadze
+surename="kvinikadze"
+print(my surename is {"kvinikadze"})
+
+my fav animal is a cameleon
+animal="cameleon"
+print(my fav animal is a {animal})
+
+i like basketball
+sport="basketball"
+print("i like {sport} ")
+
+i live in georgia in tbilisi
+citi="georgia"
+cauntry="tbilisi"
+print(i live in{ccauntry} in tbilisi )
+
+my name is dachi , im 11 years old, my favorit color is blue
+name="dachi"
+age="11"
+fav color="biue"
+my name is {"name"}, im {"age"} years old, my 
---
+++ level4/hw/cw/classwork.py
@@ -0,0 +1,7 @@
+#input შეგვიძლია გავიგოთ მომხმარებლის სახელი,გვარი და რა მოწონს
+name=input("enter your name: ") 
+Color=input("enter favorit Color:")
+print("blue")
+#f-string aris operatori rititac shegizlia teksti martivad  chaanacvlo
+age=11
+print (" im{"age"}years old:")
