9758f966384c2b64943234d5a708099baa14ac22
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-13 17:48:14 +0300

    second commit

---
+++ level 3/class/homework.py
@@ -0,0 +1,51 @@
+
+
+r=6
+print(r**2)
+
+print(17%5) 
+print(25%4) 
+print(30%7) 
+
+#მონაცემთა ტიპები განსაზღვრავს რა მონაცემი ინახება ცლადში
+
+#1.სტრინგი(str)
+#მთელი რიცხვი(int)
+#ლოგიკური(bool)
+#წილადი რიცხვი(float)
+
+#str tekstis monacemi
+#int tekstis tipis monacemi
+#bool logikuri monacemi = true da fales
+#float atwiladi dawiladi ricxvebi
+
+name=dachi
+age=11
+surname= kvinikadze
+
+
+his = "io"
+his =11
+his =1.44
+
+g=68
+h=4
+print(g+h)
+print(g-h)
+print(g*h)
+print(g/h)
+print(g%h)
+
+num=6
+print(num**3)
+
+"i have a headphone"=True
+"i have a Pet"=False
+
+intshi aris mteli ricxvebi da ar aris atwiladi 
+floatshi ki ar aris mteli rixvebi da aris atwiladebi
+
+price=67
+quanity=4
+print(price*quanity)
+
