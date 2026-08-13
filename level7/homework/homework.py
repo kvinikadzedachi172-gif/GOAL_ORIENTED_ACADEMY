684b988d0b827678ba8cafd6df85b7de8991af89
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-13 17:54:07 +0300

    second commit

---
+++ level7/homework/homework.py
@@ -0,0 +1,30 @@
+num1=input("enter num1:")
+num2=input("enter num2:")
+print(num1+num2)
+num1=45
+print(num1-5)
+print(40>20)
+num1=34
+num2=12
+print(num1+num2>50)
+print(num1*num2==100)
+print(num1<num2)
+num1=51
+result=num1/3
+num1=45
+num2=36
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+num1=input("enter first num:")
+print("squar")
+num1=5
+num2=4
+print(num1+num2>10)
+print(num1+num2<10)
+print(num1+num2==10)
+num1=87
+print(num1*3)
+print(num1**3)
+print(num1%5)
+print(num1//5)
