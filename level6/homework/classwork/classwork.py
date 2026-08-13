ec4424a4319b51b15531889060662f272b26f161
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-13 17:50:41 +0300

    second commit

---
+++ level6/homework/classwork/classwork.py
@@ -0,0 +1,23 @@
+num1=45
+num2=5
+num1=int(input("enter num1:45"))
+num2=int(input("enter num2:5"))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+print(num1**num2)
+print(num1//num2)
+print(4+5)
+print(6-4)
+print(10*45)
+print(56/8)
+print(56**8)
+print(64%5)
+print(52//8)
+age=input("enter youre age:11")
+name=input("enter youre name:dachi")
+age=11
+name="dachi"
+print(f"my name is {name}, my age is {age}")
---
+++ level6/homework/homework.py
@@ -0,0 +1,31 @@
+num1=29
+num2=35
+num1=int(input("enter num1:39"))
+num2=int(input("enter num2:35"))
+print(34+78)
+print(45-27)
+print(30*57)
+print(56/8)
+print(34%4)
+print(6**4)
+print(45//6)
+name=input("enter name: dachi")
+age=input("enter youre age:11")
+name="dachi"
+age=11
+print(F"hello, my name is {name}, im {age}years old")
+color="blue"
+print(f"my favorit color is {color}")
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+print(num1**2)
+print(num2**3)
+name="dachi"
+surename="kvinikadze"
+country="georgia"
+print(f"my name is {name} {surename} and i live in {country}")
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
