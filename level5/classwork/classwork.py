9d31738e4d0fe329e3cafc791b098091df66afd0
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-13 17:49:34 +0300

    second commit

---
+++ level5/classwork/classwork.py
@@ -0,0 +1,7 @@
+print(type("name"))
+age=int(input("enter youre age"))
+hieght=float(input("enter your hieght"))
+name="dachi"
+age=11
+hieght=1.56
+print( "im {age} years old,my name is {name} ,my hireght ism{hieght} ")
