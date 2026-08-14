17d618681ea6dece45595ace398cc9d5fb734dcb
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-14 10:39:41 +0300

    second commit

---
+++ level8/homework/homework.py
@@ -0,0 +1,28 @@
+age=int(input("enter age:"))
+print(age==18)
+print(age>18)
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print(num1 and num2>10)
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print(num1>100)
+print(num2>100)
+is_not_weekend=False
+is_the_weekend=True
+print(is_not_weekend)
+print(is_the_weekend)
+age=19
+print(age>=16)
+age=61
+print(age>=60)
+print(age>=16 and age>=60)
+has_invitation=True
+has_ticket=False
+print(has_ticket or has_invitation)
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print(num1>num2)
+print(num2>10)
+age=int(input("enter youre age:"))
+print(age>=13 and age<=18)
