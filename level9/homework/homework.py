83e65e3c34fda8f2b9aa7d1a2bf83d844f003684
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-14 10:43:58 +0300

    second commit

---
+++ level9/homework/homework.py
@@ -0,0 +1,26 @@
+age=int(input("enter age:"))
+
+if age >= 18:
+    print("you are adolt")
+
+
+password=int(input("enter password:"))
+if password == 567890:
+    print("password corect")
+point=int(input("enter youre point:"))
+if point >= 90:
+    print("point more then 90")
+temptur=int(input("enter temptur:"))
+if temptur <= 0:
+    print("its freezing aoutside")
+name=int(input("enter name:"))
+if name=="giorgi":
+    print("მოგესალმებით giorgi")
+num=int(input("enter num:"))
+if num=="ლუწი":
+    print("ლუწი რიცხვია")
+
+even=int(input("enter even number:"))
+if even==even:
+    print("ლუწი რიცხვია")
+
