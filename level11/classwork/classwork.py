2abb05db83b5a483076f35fa5c12d017f41cb07a
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-14 10:46:17 +0300

    second commit

---
+++ level11/classwork/classwork.py
@@ -0,0 +1,23 @@
+age=int(input('enter youre age:'))
+if age > 18:
+    if age > 65:
+        print('adolt')
+        print('pensioneri')
+else:
+    print('minor')  
+
+num=int(input('enter even or kent:'))     
+if num=='even':
+    if num=='kent':
+        print('positv even num')
+    else:
+        print("არ არის დადებითი")
+        print("positiv kent")
+else:
+    print("its not positiv")
+
+username=int(input("enter username:"))
+password=int (input("enter pssword:"))
+if"username is corect":
+    if"password is corect":
+        print
