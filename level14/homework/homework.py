0548082b550bc140ad6eef5a1252702a9a69306a
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-19 16:21:57 +0300

    second commit

---
+++ level14/homework/homework.py
@@ -0,0 +1,25 @@
+for u in range(0,100,2):
+ if 1%2==0:
+     total+=1
+     print(total)
+
+n=int(input("enter number:"))
+for n in range(1,n+1):
+    print(n)
+
+w=int (input("enter num:"))
+
+e=int (input("enter num:"))
+
+r=int (input("enter num:"))
+
+t=int (input("enter num:"))
+
+y=int (input("enter num:"))
+
+for w in range(1,10):
+     print(w+e+r+t+y)
+
+max_num=int(input("enter num:"))
+for i in range(5):
+    
