6de60ae3dc96be043caa9e4fe7d1c8003bf4616a
kvinikadzedachi172-gif <kvinikadzedachi172@gmail.com>
2026-08-28 17:54:32 +0300

    second commit

---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,34 @@
+for i in range(1,20):
+    if i%2==0:
+        continue
+    print(i)
+    i+=1
+
+for i in range(1,45):
+    if i%3==0:
+        continue
+    print(i)
+    i+=1
+
+for i in range(1,50) :
+    if i == 20 :
+        continue
+    if i == 30:
+        continue
+    if i == 40:
+        continue
+    print(i)
+    i+=1
+
+for i in range(5):
+    num=int (input("enter num:"))
+    if num<0:
+        continue
+    print(num)
+    i+=1
+
+for i in range(1,100):
+    if i%5==0:
+        continue
+    print(i)
+    i+=1
---
+++ level 16/homework/homework.py
@@ -0,0 +1,32 @@
+for i in range(1,15):
+    if i==7:
+        continue
+    print(i)
+    i+=1
+
+
+for i in range(2,30):
+    if i%2==0:
+        continue
+    print(i)
+    i+=1
+
+for i in range(7):
+    num=int (input("enter num:"))
+    if num<0:
+        print(num)
+        i+=1
+  
+for i in range(1,40):
+    if  i%4==0:
+        continue
+    print(i)    
+    i+=1
+
+if i in range(10):
+    num=int(input("enter num:"))
+    if num==0:
+        print(num)
+        i+=1
+
+        
